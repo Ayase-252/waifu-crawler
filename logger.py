4f6568bd0d4e3a20add0a394e262272d272be7ae
Ayase-252 <bitdqy@hotmail.com>
2016-11-14 21:44:11 +0800

    improve code style

--- logger.py
+++ logger.py
@@ -5,7 +5,9 @@ Custom log configuration
 import logging
 
 FORMAT = "[%(asctime)s][%(module)s][%(thread)d:%(threadName)s][%(levelname)s]:%(message)s"
-logging.basicConfig(filename='run.log', filemode='w', level="DEBUG",format=FORMAT)
+logging.basicConfig(filename='run.log', filemode='w',
+                    level="DEBUG", format=FORMAT)
+
 
 def getLogger(logger_path):
     return logging.getLogger(logger_path)
