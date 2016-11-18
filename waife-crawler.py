293634b01801790ce3b572bd31d897aa8ee7a482
Ayase-252 <bitdqy@hotmail.com>
2016-11-18 17:35:32 +0800

    add argparse

--- waife-crawler.py
+++ waife-crawler.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+import argparse
 import sys
 
 from command import waife_crawler
 
 if __name__ == '__main__':
-    waife_crawler.run()
+    parser = argparse.ArgumentParser(description='Web crawler for pictures of '
+                                     'adorable waifes. :)')
+    parser.add_argument('-t', '--threshold', type=int, dest='threshold',
+                        default=100, help='Score threshold, Picture scoring '
+                        'higher than threshold will be downloaded. '
+                        '(Default 100)')
+    sys_args = parser.parse_args(sys.argv[1:])
+    waife_crawler.run(sys_args.threshold)
